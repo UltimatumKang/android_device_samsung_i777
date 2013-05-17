b459975e65cf7eaa88920c0ea799c6e5ef8d981b
David Hou <david.hou314@gmail.com>
2013-05-17 15:47:58 -0400

    stuff

--- i777.mk
+++ i777.mk
@@ -87,4 +87,4 @@ PRODUCT_COPY_FILES += \
 	$(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
 
 $(call inherit-product-if-exists, vendor/samsung/i777/i777-vendor.mk)
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
