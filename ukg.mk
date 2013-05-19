8199f95a9e9608646017058fff0bf4d73030d373
David Hou <david.hou314@gmail.com>
2013-05-18 20:51:22 -0700

    This seems to work

--- ukg.mk
+++ ukg.mk
@@ -2,14 +2,14 @@
 $(call inherit-product, device/samsung/i777/full_i777.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/ukg/config/common.mk)
 
 # Inherit GSM common stuff
-$(call inherit-product, vendor/aosp/config/gsm.mk)
+$(call inherit-product, vendor/ukg/config/gsm.mk)
 
 # Setup device specific product configuration.
 PRODUCT_DEVICE := i777
-PRODUCT_NAME := xylon_i777
+PRODUCT_NAME := ukg_i777
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := SGH-I777
 
@@ -21,5 +21,5 @@ PRODUCT_PACKAGES += \
     Thinkfree
 
 PRODUCT_COPY_FILES += \
-   vendor/aosp/prebuilt/hybrid_hdpi.conf:system/etc/beerbong/properties.conf \
-   vendor/aosp/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+   vendor/ukg/prebuilt/hybrid_hdpi.conf:system/etc/beerbong/properties.conf \
+   vendor/ukg/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
